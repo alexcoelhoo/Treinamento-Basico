d05f24d74525ff4cf501169608d0c648a22c6163
Alex Coelho <alexcoelhoo@outlook.com>
2013-10-05 16:00:14 -0300

    triangulo operador logico

--- Aula_1/Aula_1/Program.cs
+++ Aula_1/Aula_1/Program.cs
@@ -10,15 +10,22 @@ namespace Aula_1
     {
         static void Main(string[] args)
         {
-            List<string> animais = new List<string>();
-            animais.Add("cachorro");
-            animais.Add("gato");
-            animais.Add("galinha");
+            Console.WriteLine("Informe os lados de um triangulo: ");
+            int lado1 = Convert.ToInt32(Console.ReadLine());
+            int lado2 = Convert.ToInt32(Console.ReadLine());
+            int lado3 = Convert.ToInt32(Console.ReadLine());
 
-            foreach (string animal in animais)
+            if ((lado1 == lado2) && (lado2 == lado3))
             {
-                Console.WriteLine(animal);
+                Console.WriteLine("Triangulo equilatero");
             }
+
+            else if ((lado1 == lado2) || (lado2 == lado3) || (lado3 == lado1))
+            {
+                Console.WriteLine("Triangulo isosceles");
+                                
+            }
+            else Console.WriteLine("Triangulo escaleno");
           
             Console.ReadKey();
         }
