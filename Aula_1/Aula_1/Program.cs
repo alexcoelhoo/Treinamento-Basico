8c8efbc6054f0b21d9c79f3e856058b97f8a1270
Alex Coelho <alexcoelhoo@outlook.com>
2013-10-05 15:25:33 -0300

    Apagando codigo antigo

--- Aula_1/Aula_1/Program.cs
+++ Aula_1/Aula_1/Program.cs
@@ -10,21 +10,7 @@ namespace Aula_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Qual seu nome:");
-            string nome = Console.ReadLine();
-            Console.WriteLine("Bom dia " + nome);
-
-            Console.WriteLine("Informe um numero: ");
-            int numero = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Informe outro numero: ");
-            int numero2 = Convert.ToInt32(Console.ReadLine());
-            int resultado = numero + numero2;
-            Console.WriteLine("Numero: " + resultado);
-
-
             
-
-            Console.ReadKey();
         }
     }
 }
