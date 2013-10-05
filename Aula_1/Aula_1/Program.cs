d921d358e3551a2b8686b12bf4e952dafb48761d
Alex Coelho <alexcoelhoo@outlook.com>
2013-10-05 15:23:08 -0300

    Primeiro Exercicio
    Qual é o seu nome
    Soma de dois numeros

---
+++ Aula_1/Aula_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual seu nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Bom dia " + nome);
+
+            Console.WriteLine("Informe um numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe outro numero: ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            int resultado = numero + numero2;
+            Console.WriteLine("Numero: " + resultado);
+
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
